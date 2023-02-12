4e04f015ed9717a8b5113a976e086ecf7c420238
Letigaro <113176418+Letigaro@users.noreply.github.com>
2023-02-12 22:08:33 +0100

    modulo2

--- Modulo_2/Laboratory/app.js
+++ Modulo_2/Laboratory/app.js
@@ -5,28 +5,27 @@ var hotel= {
 rating={
     puntuacion:"",
     stars:"&#9734;",
+    stars2:"&#9733; ",
     anonymous:"",
 }
 
-checkbox={
-    checked:"checked",
-    nochecked:"",
-}
 
+// Nombre de hotel y ubicacion
 hotel.name=prompt("Escriba el nombre del hotel, por favor");
-document.getElementById("hotelname").innerHTML="Hotel" + hotel.name;
+document.getElementById("hotelname").innerHTML="Hotel " + hotel.name + "<br>";
 hotel.location=prompt("Escriba la ubicación del hotel, por favor");
-document.getElementById("hotellocation").innerHTML=hotel.location;
+document.getElementById("hotellocation").innerHTML= "Ubicado en " + hotel.location;
 
+// Reseña de hotel
 
-rating.puntuacion=prompt("¿Qué puntuación le da al hotel?");
-var puntuacionfinal=rating.stars.repeat(rating.puntuacion);
+rating.puntuacion=prompt("¿Qué puntuación le da al hotel del 1 al 5?");
+var puntuacionfinal=rating.stars2.repeat(rating.puntuacion)+rating.stars.repeat(5-rating.puntuacion);
 document.getElementById("stars").innerHTML= puntuacionfinal;
 
-
+// Reseña del hotel
+ 
 rating.anonymous=confirm("¿Quiere que su reseña sea anónima?");
-console.log(checkbox.checked);
-document.getElementById("review").attributes=checkbox.checked;
+document.getElementById("review").checked = rating.anonymous;
 
 
 
