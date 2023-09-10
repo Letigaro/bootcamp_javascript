d8c3b731220d25088a7c9576c485b8214de33b07
Letigaro <113176418+Letigaro@users.noreply.github.com>
2023-09-10 11:16:38 +0200

    update modulo 6

---
+++ Modulo_6/Algoritmos I/app.js
@@ -0,0 +1,30 @@
+var productList=[
+    {
+        product: "Jamon",
+        units: 1,
+        price: 9.99
+    },
+    {
+        product: "queso",
+        units: 6,
+        price: 4.99
+    },
+    {
+        product: "vino",
+        units: 12,
+        price: 2.99
+    },
+];
+
+// Dada una lista de productos donde se describe la cantidad y el precio unitario de cada producto,
+// aplica un descuento del 5% para aquellos productos que superen 5 unidades, y un descuento adicional
+// del 5% si el precio total de un producto es igual o supera 50€.
+
+// Precio de cada producto
+
+for (x=0; x<productList; x++){
+    let precioProducto=
+}
+// Precio total de la compra
+// si supera 5 unidades, 5% descuento en el coste del producto
+// Si el precio de un producto supera 50€ se le hará descuento sobre ese producto
